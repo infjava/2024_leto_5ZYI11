11ea01dd8622d69b4b422f0039dcd9edf380e6dd
Jan Janech <jan.janech@fri.uniza.sk>
2025-04-04 08:28:38 +0200

    07 do klietky sa da vlozit aj cele cislo?

--- 07/zoo/src/Main.java
+++ 07/zoo/src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        // klietka na levy
         var klietkaNaLeva = new Klietka();
         klietkaNaLeva.vlozZviera(new Lev("Simba"));
         klietkaNaLeva.vypis();
 
+        // klietka na mysi
         var klietkaNaMys = new Klietka();
         klietkaNaMys.vlozZviera(new Mys());
         klietkaNaMys.vypis();
+
+        // klietka na cele cisla
+        var divnaKlietka = new Klietka();
+        divnaKlietka.vlozZviera(5);
+        divnaKlietka.vypis();
     }
 }
