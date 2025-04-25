7f4045cfa5596c7a24b5d5dccea9a1bfbf207e55
Jan Janech <jan.janech@fri.uniza.sk>
2025-04-25 08:27:30 +0200

    09 zakladne okno

--- 09/vtipneGUI/src/Main.java
+++ 09/vtipneGUI/src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var okno = new JFrame("Otázka");
+
+        okno.setLayout(new GridLayout(2, 2));
+
+        okno.add(new JLabel("Chceš úspešne dokončiť Informatiku 2?"));
+        okno.add(new JLabel());
+        okno.add(new JButton("Áno"));
+        okno.add(new JButton("Nie"));
+
+        okno.setVisible(true);
     }
 }
