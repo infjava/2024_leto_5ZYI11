ed89e20609f94c9429c6a6f7918bee91e19b68f8
Jan Janech <jan.janech@fri.uniza.sk>
2025-04-25 10:06:25 +0200

    09 zoznam studentov v JListe

--- 09/studijnaSkupina/src/HlavneOkno.java
+++ 09/studijnaSkupina/src/HlavneOkno.java
@@ -2,16 +2,25 @@ import javax.swing.*;
 
 public class HlavneOkno {
     private final JFrame okno;
+    private final DefaultListModel<Student> studenti;
+
     private JTextField meno;
     private JTextField priezvisko;
     private JTextField vek;
     private JButton pridaj;
     private JButton odstran;
     private JButton uprav;
-    private JList zoznam;
+    private JList<Student> zoznam;
     private JPanel hlavnyPanel;
 
     public HlavneOkno() {
+        this.studenti = new DefaultListModel<Student>();
+        this.studenti.addElement(new Student("Ferko", "Mrkvicka", 1850));
+        this.studenti.addElement(new Student("Jozko", "Pagacik", 2010));
+        this.studenti.addElement(new Student("Andy", "Lyzica", 2023));
+
+        this.zoznam.setModel(this.studenti);
+
         this.okno = new JFrame("Zoznam študentov");
         this.okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.okno.add(this.hlavnyPanel);
